917ad0e7c1e3b6ab1875de477b2e535b7a60bc9d
Omar Jabbour <128647309+omaarjb@users.noreply.github.com>
2024-05-04 19:01:08 +0100

    update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Cette fonction récupère les valeurs saisies par l'utilisateur pour définir le nombre de variables et de contraintes
 function get_input() {
+  // Récupérer le nombre de variables à partir de l'élément HTML
   let num_vars = parseInt(document.getElementById("num-vars").value);
+  // Afficher la section pour entrer la fonction objectif
   let objectiveInputs = document.getElementById("objective-inputs");
   objectiveInputs.innerHTML =
     "<div class='objective-container border p-3 rounded'><h4 class='title'>La fonction objectif max(Z)</h4></div>";
 
   let container = objectiveInputs.querySelector(".objective-container");
 
-  // Add <br> tag after the border
-
+  // Ajouter des éléments d'entrée pour chaque coefficient de la fonction objectif
   for (let i = 0; i < num_vars; i++) {
     let inputGroup = document.createElement("div");
     inputGroup.classList.add("input-group");
@@ -21,16 +23,18 @@ function get_input() {
   }
   objectiveInputs.innerHTML += "<br>";
 
+  // Récupérer le nombre de contraintes à partir de l'élément HTML
   let num_constraints = parseInt(
     document.getElementById("num-constraints").value
   );
   let constraintInputs = document.getElementById("constraint-inputs");
   constraintInputs.innerHTML = "";
 
-  // Create a container div with Bootstrap border class
+  // Créer un conteneur avec des bordures Bootstrap pour les entrées des contraintes
   let containerDiv = document.createElement("div");
   containerDiv.classList.add("border", "p-3", "rounded");
 
+  // Ajouter des entrées pour chaque contrainte
   for (let i = 0; i < num_constraints; i++) {
     let constraintRow = document.createElement("div");
     constraintRow.classList.add("input-group");
@@ -65,7 +69,9 @@ function get_input() {
   constraintInputs.appendChild(containerDiv);
 }
 
+// Cette fonction implémente l'algorithme simplex pour résoudre le problème d'optimisation linéaire
 function simplex(c, A, b) {
+  // Préparer le tableau initial du simplexe
   A.forEach((row) => row.push(...Array(A.length).fill(0)));
   c.push(...Array(A.length).fill(0));
 
@@ -89,20 +95,24 @@ function simplex(c, A, b) {
       break;
     }
 
+    // Sélectionner la colonne pivot
     let pivot_column = tableau[tableau.length - 1]
       .slice(0, -1)
       .indexOf(Math.max(...tableau[tableau.length - 1].slice(0, -1)));
 
+    // Calculer les rapports pour sélectionner la ligne pivot
     let ratios = tableau
       .slice(0, -1)
       .map((row) => row[row.length - 1] / row[pivot_column]);
     ratios = ratios.map((value, index) => (value < 0 ? Infinity : value));
     let pivot_row = ratios.indexOf(Math.min(...ratios));
 
+    // Vérifier si l'optimisation est impossible
     if (ratios[pivot_row] === Infinity) {
       return [null, null];
     }
 
+    // Mettre à jour les variables de base et effectuer les opérations du simplexe
     basic_variables[pivot_row] = pivot_column;
 
     let pivot_value = tableau[pivot_row][pivot_column];
@@ -118,18 +128,25 @@ function simplex(c, A, b) {
     iteration++;
   }
 
+  // Récupérer la solution optimale et la valeur optimale
   let optimal_solution = Array.from({ length: c.length }, () => 0);
+  let slack_variables = [];
+
   basic_variables.forEach((col, index) => {
     if (col < c.length) {
       optimal_solution[col] = tableau[index][tableau[0].length - 1];
+    } else {
+      slack_variables.push(tableau[index][tableau[0].length - 1]);
     }
   });
 
   let optimal_value = -tableau[tableau.length - 1][tableau[0].length - 1];
-  return [optimal_solution, optimal_value];
+  return [optimal_solution, optimal_value, slack_variables];
 }
 
+// Cette fonction est appelée lorsqu'un utilisateur clique sur le bouton "Résoudre"
 function solve() {
+  // Récupérer les coefficients de la fonction objectif et les contraintes saisies par l'utilisateur
   let num_vars = parseInt(document.getElementById("num-vars").value);
   let c = [];
   for (let i = 0; i < num_vars; i++) {
@@ -171,22 +188,53 @@ function solve() {
     b.push(rhs);
   }
 
-  let [variables, obj] = simplex(c, A, b);
+  // Appeler la fonction simplex pour résoudre le problème
+  let [variables, obj, slackVariables] = simplex(c, A, b);
   let output = document.getElementById("output");
 
+  // Afficher la solution ou un message d'erreur si l'optimisation est impossible
   if (variables === null && obj === null) {
     output.innerHTML = `<div class="alert alert-danger" role="alert">Optimization is not possible!</div>`;
     return;
   }
 
+  // Générer le HTML pour afficher la solution
   let variablesHtml = "";
   for (let i = 0; i < num_vars; i++) {
-    variablesHtml += `<tr><th scope="row">x${i + 1}</th><td>${
-      variables[i]
-    }</td></tr>`;
+    // Pour chaque variable dans la solution optimale
+    if (variables[i] !== 0) {
+      // Vérifier si la variable est une variable de base ou non
+      // Si la variable est une variable de base, l'ajouter à la sortie avec sa valeur et la catégorie VB (Variable de Base)
+      variablesHtml += `<tr class="base-variable"><th scope="row">x${
+        i + 1
+      }</th><td>${variables[i]}</td><td class="variable-category">VB</td></tr>`;
+    } else {
+      // Sinon, l'ajouter à la sortie avec sa valeur et la catégorie VHB (Variable Hors Base)
+      variablesHtml += `<tr class="non-base-variable"><th scope="row">x${
+        i + 1
+      }</th><td>${
+        variables[i]
+      }</td><td class="variable-category">VHB</td></tr>`;
+    }
   }
 
-  output.innerHTML = `
+  // Générer le HTML pour afficher les variables d'écart
+  let slackVariablesHtml = "";
+  for (let i = 0; i < num_constraints; i++) {
+    let slackVar = i < slackVariables.length ? slackVariables[i] : 0;
+    if (slackVar !== 0) {
+      slackVariablesHtml += `<tr class="base-variable"><th scope="row">e${
+        i + 1
+      }</th><td>${slackVar}</td><td class="variable-category">VB</td></tr>`;
+    } else {
+      slackVariablesHtml += `<tr class="non-base-variable"><th scope="row">e${
+        i + 1
+      }</th><td>${slackVar}</td><td class="variable-category">VHB</td></tr>`;
+    }
+  }
+
+  // Afficher la solution dans le HTML
+  output.innerHTML = `<br><br>
         <div class="table-container">
             <h2 class="text-center mb-3 title">Solution optimale</h2>
             <table class="table table-bordered">
@@ -194,20 +242,25 @@ function solve() {
                     <tr>
                         <th scope="col">Variable</th>
                         <th scope="col">Valeur</th>
+                        <th scope="col">Catégorie</th>
                     </tr>
                 </thead>
                 <tbody>
                     ${variablesHtml}
+                    ${slackVariablesHtml}
                     <tr class="red-row"> 
                         <th scope="row">Z : la valeur objectif</th>
                         <td>${obj}</td>
+                        <td>N/A</td>
                     </tr>
                 </tbody>
             </table>
+            <br>
         </div>
     `;
 }
 
+// Écouter les changements sur le nombre de variables et de contraintes pour mettre à jour les entrées
 document.getElementById("num-vars").addEventListener("change", get_input);
 document
   .getElementById("num-constraints")
